bfe5f8192070e42511fa71a75ca6ad0207f7b5ea
Gaurav Sablok <gauravcodepro@gmail.com>
2024-09-25 13:41:26 +0200

    codecreatede
    finished writing the code for the ldapsswd and ldapadd. Each command has its own options. Adding all the other ldap files creation and json structs

--- README.md
+++ README.md
@@ -3,4 +3,47 @@ golang-ldap
 - configures and runs LDAP servers
 - writes and also allows for the update of the LDFI
 
+```
+[gauravsablok@ultramarine]~/Desktop/codecreatede/golang/golang-ldap% \
+go run main.go -h
+System wide installation and maintainence of the the LDAP for the high performance computing system
+
+Usage:
+  command [command]
+
+Available Commands:
+  completion  Generate the autocompletion script for the specified shell
+  help        Help about any command
+  ldap
+  ldif
+  pass
+
+Flags:
+  -h, --help   help for command
+
+Use "command [command] --help" for more information about a command.
+
+[gauravsablok@ultramarine]~/Desktop/codecreatede/golang/golang-ldap% \
+go run main.go ldif -h
+install and created the new user using the ldif files
+
+Usage:
+  command ldif [flags]
+
+Flags:
+  -u, --dnentryuser string   entry point for the username (default "specify the dn entry for the user")
+  -h, --help                 help for ldif
+  -l, --pathldif string      user ldif file (default "path to the ldif file for the new user")
+
+[gauravsablok@ultramarine]~/Desktop/codecreatede/golang/golang-ldap% \
+go run main.go ldap -h
+install the system wide ldap installtion
+
+Usage:
+  command ldap installation [flags]
+
+Flags:
+  -h, --help   help for ldap
+```
+
 Gaurav Sablok
--- main.go
+++ main.go
@@ -20,8 +20,16 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	passwd        string
+	usernameentry string
+	dnentry       string
+	userdn        string
+	pathldif      string
+)
+
 func main() {
-	if err := rooCmd.Execute(); err != nil {
+	if err := rootCmd.Execute(); err != nil {
 		fmt.Println()
 	}
 }
@@ -39,26 +47,60 @@ var ldapCmd = &cobra.Command{
 
 var ldifCmd = &cobra.Command{
 	Use:  "ldif",
-	Long: "install and created the ldif files",
-	Run:  ldifInstall,
+	Long: "install and created the new user using the ldif files",
+	Run:  ldifUser,
 }
 
 var passCmd = &cobra.Command{
 	Use:  "pass",
-	Long: "password enable for the LDAP system",
-	Run:  passInstall,
+	Long: "password enable for the LDAP system for the new user",
+	Run:  passUser,
 }
 
-// rootCmd.AddCommand(ldapCmd)
-// rootCmd.AddCommand(ldifCmd)
-// rootCmd.AddCommand(passCmd)
+func init() {
+	passCmd.Flags().
+		StringVarP(&passwd, "pass", "p", "password that you want to give", "password for the user group")
+	passCmd.Flags().
+		StringVarP(&usernameentry, "username-entry", "u", "username entry defined for the grop", "username-entry")
+	passCmd.Flags().
+		StringVarP(&dnentry, "dnentry", "e", "specify the dn entry", "distinguished name to authenticate the server")
+	ldifCmd.Flags().
+		StringVarP(&userdn, "dnentryuser", "u", "specify the dn entry for the user", "entry point for the username")
+	ldifCmd.Flags().
+		StringVarP(&pathldif, "pathldif", "l", "path to the ldif file for the new user", "user ldif file")
+	rootCmd.AddCommand(ldapCmd)
+	rootCmd.AddCommand(ldifCmd)
+	rootCmd.AddCommand(passCmd)
+}
 
 func ldapInstall(cmd *cobra.Command, args []string) {
-	out, err = exec.Command("sudo", "dnf", "install", "-y", "openldap", "openldap-clients", "openldap-servers").Output()
+	out, err := exec.Command("sudo", "dnf", "install", "-y", "openldap", "openldap-clients", "openldap-servers").
+		Output()
 	if err != nil {
 		log.Fatal(err)
 		fmt.Println(string(out))
 	}
 }
 
-// few more functions to write to add to the same.
+func ldifUser(cmd *cobra.Command, args []string) {
+	arg1 := passwd
+	arg2 := usernameentry
+	arg3 := dnentry
+
+	install, err := exec.Command("ldappassswd", "-s", arg1, "-W", "-D", arg3, "-X", arg2).Output()
+	if err != nil {
+		log.Fatal(err)
+		fmt.Println(install)
+	}
+}
+
+func passUser(cmd *cobra.Command, args []string) {
+	args1 := userdn
+	args2 := pathldif
+
+	userinstall, err := exec.Command("ldapadd", "-x", "-W", "-D", args1, "-f", args2).Output()
+	if err != nil {
+		log.Fatal(err)
+		fmt.Println(userinstall)
+	}
+}
